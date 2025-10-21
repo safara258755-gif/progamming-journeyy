6dd095eae037962daee21a113080e0b12b16ce00
safara258755-gif <safara258755@student.smkn1kandeman.sch.id>
2025-10-21 14:01:03 +0700

    Add files via upload

---
+++ tugas 1 kharin/tugas 1 kharin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_1_kharin
+{
+    internal class Program
+    {
+        // Fungsi Kali menerima tiga bilangan dan menampilkan hasil perkalian
+        static void Kali(int a, int b, int c)
+        {
+            int hasil = a * b * c;
+            Console.WriteLine("Hasil perkalian: " + hasil);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan bilangan pertama: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan bilangan kedua: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Masukkan bilangan ketiga: ");
+            int z = int.Parse(Console.ReadLine());
+
+            Kali(x, y, z); // Panggil fungsi
+        }
+    }
+}
+        
+    
+
