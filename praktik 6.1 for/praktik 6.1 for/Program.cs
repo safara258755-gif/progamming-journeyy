5ddeca8a20f08213340098d4a1b28040b8fd5a80
safara258755-gif <safara258755@student.smkn1kandeman.sch.id>
2025-10-21 09:45:29 +0700

    Add files via upload

---
+++ praktik 6.1 for/praktik 6.1 for/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._1_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("perulangan untuk menampilkan angka 1 sampai 10:");
+
+            // Perulangan for dimulai dari sini
+            for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang
+                Console.WriteLine("Angka ke-" + i);
+
+            }
+            Console.WriteLine("\nPerulangan selesai.");
+        }
+    }
+}
