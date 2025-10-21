89168195c15d8c97342a31e2220d6480e0d37744
safara258755-gif <safara258755@student.smkn1kandeman.sch.id>
2025-10-21 09:48:01 +0700

    Add files via upload

---
+++ praktik 6.3/praktik 6.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi variabel untuk menyimpan input
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga anda mengetik 'keluar . ");
+
+            //Perulangan selama isi variabel inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar")
+            {
+                Console.WriteLine("\nketik sesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca input dari user
+
+                Console.WriteLine("Anda mengetik: " + inputUser);
+
+            }
+
+            Console.WriteLine("\nProgram selesai. Terima Kasih!");
+        }
+    }
+}
