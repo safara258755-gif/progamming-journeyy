d5a77db9f61be029c006f21906b489c022f1a828
safara258755-gif <safara258755@student.smkn1kandeman.sch.id>
2025-10-21 09:51:55 +0700

    Add files via upload

---
+++ praktik 6.6 for/praktik 6.6 for/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._6_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka;
+            //Blok 'do' akan dieksekusi minimal satu kali.
+            do
+            {
+                Console.WriteLine("masukkan angka genap: ");
+                // Mencoba membaca input dari pengguna
+                if (!int.TryParse(Console.ReadLine(), out angka))
+                {
+                    Console.WriteLine("input tidak valid."
+                }
+            }
+        }
+    }
+}
---
+++ praktik 6.6 for/praktik 6.6 for/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("praktik 6.6 for")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("praktik 6.6 for")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("936e69f7-608c-4ead-a654-b0996e6d0b57")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
