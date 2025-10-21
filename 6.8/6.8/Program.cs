d429a259f54fcadcd4e6ccd78bf3a9790d6ae9a2
safara258755-gif <safara258755@student.smkn1kandeman.sch.id>
2025-10-21 09:54:09 +0700

    Add files via upload

---
+++ 6.8/6.8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangPilihan;
+
+            do
+            {
+                // --- Blok Proses akan diulang ---
+                Console.WriteLine("----------------------------------");
+                Console.Write("Masukkan nama anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Hallo, {nama}! Proses telah selesai");
+                // --------------------------------------
+                // Pertanyaan untuk mengulang 
+                Console.Write("Apakah Anda ingin mengulang lagi (ya/tidak): ");
+                ulangPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris kosong untuk kerapian
+
+                // Kondisi diperiksa di akhir: Ulangi selam pengguna mengetik "ya"
+            } while (ulangPilihan == "ya");
+            Console.WriteLine("Terima Kasih. Program selesai.");
+        }
+    }
+}
