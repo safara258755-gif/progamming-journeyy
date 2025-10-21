cac2ed2ed3de997a5308860c1ff8dd41448f48d8
safara258755-gif <safara258755@student.smkn1kandeman.sch.id>
2025-10-21 14:02:45 +0700

    Add files via upload

---
+++ tugas fungsi ratarata/tugas fungsi ratarata/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_fungsi_ratarata
+{
+    internal class Program
+    {
+        // Fungsi menghitung rata-rata dari 3 nilai
+        static double RataRata(double a, double b, double c)
+        {
+            double hasil = (a + b + c) / 3;
+            return hasil; // mengembalikan nilai
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai pertama: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai kedua: ");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai ketiga: ");
+            double n3 = double.Parse(Console.ReadLine());
+
+            // Panggil fungsi dan simpan hasilnya
+            double hasilRata = RataRata(n1, n2, n3);
+
+            // Tampilkan hasil ke layar
+            Console.WriteLine("Hasil rata-rata: " + hasilRata);
+        }
+    }
+}
+
+    
+
