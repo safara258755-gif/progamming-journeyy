2922268b625efdb2ad1341e30a5596f4aec371cc
safara258755-gif <safara258755@student.smkn1kandeman.sch.id>
2025-10-21 09:46:55 +0700

    Add files via upload

---
+++ praktik 6.2 for/praktik 6.2 for/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._2_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabel untuk menyimpan hasil penjumlahan
+            int total = 0;
+
+            Console.WriteLine("menghitung jumlah angka dari 1 sampai 5:");
+
+            //perulangan for dari1 sampai 5
+            for (int i = 1; i <= 5; i++)
+            {
+                // Tambahkan nilai i saat ini ke variabel total
+                total = total + i; // Bisa juga ditulis sebagai: total += i;
+
+
+                //Tambahkan proses penjumlahan di setiap langkah
+                Console.WriteLine("Menambahkan " + i + ", total sementara = " + total);
+            }
+
+              // Tampilkan hasil akhir setelah perulangan selesai
+              Console.WriteLine("\nHasil akhir penjumlahan adalah: " + total);
+        }
+    }
+}
